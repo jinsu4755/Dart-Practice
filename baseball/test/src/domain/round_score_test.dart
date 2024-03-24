47d3bb7185894bcce05c972bb9e27c259bffec31
jinsu4755 <parkjinsu4755@gmail.com>
2024-03-24 17:52:13 +0900

    test: Add RoundScore toString Test

--- baseball/test/src/domain/round_score_test.dart
+++ baseball/test/src/domain/round_score_test.dart
@@ -50,4 +50,15 @@ void main() {
               "message", "잘못된 스코어 입니다. 3스트라이크에서는 볼이 존재할 수 없습니다.")));
     });
   });
+
+  group("올바른 스코어를 가진 경우 정해진 문자열로 출력된다.", () {
+    test('strike 가 0이고 ball이 0 이면 낫싱이다.',
+        () => expect(RoundScore(strike: 0, ball: 0).toString(), equals("낫싱")));
+    test('strike 가 0이고 ball이 1 이면 1볼이다.',
+        () => expect(RoundScore(strike: 0, ball: 1).toString(), equals("1볼")));
+    test('strike 가 1이고 ball이 0 이면 1스트라이크이다.',
+        () => expect(RoundScore(strike: 1, ball: 0).toString(), equals("1스트라이크")));
+    test('strike와 ball이 존재하면 볼이 스트라이크 보다 먼저 위치한다.',
+        () => expect(RoundScore(strike: 1, ball: 1).toString(), equals("1볼 1스트라이크")));
+  });
 }
